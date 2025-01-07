6ff42d8e47607d190695e0371d11f6119b6fef5b
AyushmaanAggarwal <ayu02468@gmail.com>
2025-01-07 11:09:50 -0800

    Update to nextcloud 30

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -6,7 +6,7 @@
   # -------------------- 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud28;
+    package = pkgs.nextcloud30;
     hostName = "nextcloud.tail590ac.ts.net";
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
