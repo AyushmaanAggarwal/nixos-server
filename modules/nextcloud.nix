2bfd79d10f6cc3c68b9d3148310782a0ee05a71e
AyushmaanAggarwal <ayu02468@gmail.com>
2025-01-07 21:48:59 -0800

    Add proxied domain as trusted domain in nextcloud

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -33,6 +33,7 @@
     enable = true;
     package = pkgs.nextcloud30;
     hostName = "localhost";
+    settings.trusted_domains = [ "nextcloud.tail590ac.ts.net" ];
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
     };
