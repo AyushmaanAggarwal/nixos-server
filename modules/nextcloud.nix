2a2caf3a6389c0f698b41e33b9c708f7163e6578
AyushmaanAggarwal <ayu02468@gmail.com>
2025-01-07 19:54:36 -0800

    Remove broken let statement

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -10,24 +10,21 @@
     description = "Nextcloud User"; 
   };
  
-  let 
-    nc = pkgs.nextcloud30;
-  in
-    services.nextcloud = {
-      enable = true;
-      package = nc;
-      hostName = "nextcloud.tail590ac.ts.net";
-      extraApps = {
-        inherit (nc.packages.apps) news contacts calendar tasks;
-      };
-      extraAppsEnable = true;
-      autoUpdateApps.enable = true;
-
-      database.createLocally = true;
-      config = {
-        dbtype = "pgsql";
-        adminuser = "admin";
-        adminpassFile = "/etc/secrets/nextcloud-admin-pass";
-      };
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud30;
+    hostName = "nextcloud.tail590ac.ts.net";
+    extraApps = {
+      inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
     };
+    extraAppsEnable = true;
+    autoUpdateApps.enable = true;
+
+    database.createLocally = true;
+    config = {
+      dbtype = "pgsql";
+      adminuser = "admin";
+      adminpassFile = "/etc/secrets/nextcloud-admin-pass";
+    };
+  };
 }
