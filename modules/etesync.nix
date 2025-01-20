06f463d2b9805a80ac5a3b98953a0e393bd44aa1
AyushmaanAggarwal <ayu02468@gmail.com>
2025-01-19 17:30:29 -0800

    Set etebase server port

--- modules/etesync.nix
+++ modules/etesync.nix
@@ -6,10 +6,11 @@
   # -------------------- 
   services.etebase-server = {
     enable = true;
+    port = 8001;
     settings = {
       allowed_hosts = {
-        allowed_host2 = "etebase.tail590ac.ts.net";
         allowed_host1 = "127.0.0.1";
+        allowed_host2 = "etebase.tail590ac.ts.net";
       };
       global.secret_file = "/var/lib/etebase-server/secret_file";
     };
@@ -23,7 +24,7 @@
     enable = true;
     virtualHosts."etebase.tail590ac.ts.net".extraConfig = ''
       reverse_proxy 127.0.0.1:8001
-        header_up Host {upstream_hostport}
+        header_up Host 8001
     '';
   };
   services.tailscale.permitCertUid = "caddy";
