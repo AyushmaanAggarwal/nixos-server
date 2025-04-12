cd2a9d5a9d591d53ae7e3079aa06650b26eed3fa
AyushmaanAggarwal <ayu02468@gmail.com>
2025-04-12 11:00:23 -0700

    Removed usersdb from library

--- modules/calibre-server.nix
+++ modules/calibre-server.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 let 
-  userdb-path = "/var/lib/calibre-server";
+  userdb-path = "/var/lib/calibre-users";
 in
 {
-  # Need to generate file with `calibre-server --userdb users.sqlite --manage-users` 
+  # Need to generate file with `calibre-server --userdb /srv/calibre/users.sqlite --manage-users` 
   # if it doesn't exist already. Also copy usersdb into /var/lib/users.sqlite during installation
   system.activationScripts.script.text = ''
     mkdir ${userdb-path}
@@ -24,7 +24,7 @@ in
       userDb = "${userdb-path}/users.sqlite";
     };
     openFirewall = false;
-    libraries = [ "/var/lib/calibre-server/users.sqlite" ];
+    libraries = [ "/var/lib/calibre-server" ];
   };
   # -------------------- 
   # Caddy SSL Cert
